b8c2982321bd2caf24dbd1f53e852ace201b7cb6
ThePiranha <sabah_365@hotmail.com>
2017-01-03 08:16:10 -0500

    First commit

---
+++ ListExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);            
+            list.Add(8);
+            list.Add(9);
+            list.Add(10);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+
+            list.Sort();
+
+            // Loop through List with foreach.
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n\nSecond item in list is: " + list[1]);
+
+            Console.ReadLine();
+        }
+    }
+}
